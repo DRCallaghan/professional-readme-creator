5b38753027e97898a9f525ceb2939cc90bbe73c3
DRCallaghan <dennis.callaghan87@gmail.com>
2022-07-18 16:11:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is your project's title?",
+    'What was your motivation?',
+    'Why did you build this project?',
+    'What problem does it solve?',
+    'What did you learn?',
+    'What makes your project stand out?',
+    'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    'Provide instructions and examples for use. Include screenshots as needed.',
+    'List your collaborators, if any, with links to their GitHub profiles.',
+    'If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.',
+    'If you followed tutorials, include links to those here as well.',
+    'Which license would you like to use?',
+    'What are your guidelines for contribution to this project?'
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
